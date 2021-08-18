aa7c6da3857a603bb8beea185542a0ae1a87f927
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-08-17 21:28:46 -0400

    Initial commit 2: Electric boogaloo

---
+++ titanfall2-rp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace titanfall2_rp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
