IncPlusPlus/titanfall2-rp

commit 77cdcc0bf14e1e09e95fdd1d156f470595fdf98e

Fix control flow (#47)
Avoid using Thread.sleep() for periodically updating a user's status.
Also add logging!

titanfall2-rp/PresenceUpdateThread.cs
@@ -1,0 +1,138 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Timers;
+using DiscordRPC;
+using log4net;
+
+namespace titanfall2_rp
+{
+    public class PresenceUpdateThread
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()!.DeclaringType);
+        private static System.Timers.Timer? presenceUpdateTimer;
+        private const int ProcessOpenWaitTimeInMinutes = 1;
+        private readonly DiscordRpcClient _discordRpcClient;
+        private readonly Titanfall2Api _tf2Api;
+        private readonly EventWaitHandle _userExitEvent;
+
+        public PresenceUpdateThread(DiscordRpcClient discordRpcClient, Titanfall2Api tf2Api, AutoResetEvent userExitEvent)
+        {
+            _discordRpcClient = discordRpcClient;
+            _tf2Api = tf2Api;
+            _userExitEvent = userExitEvent;
+        }
+
+        public void Run()
+        {
+            // Set up a timer that'll regularly run our method that updates the rich presence
+            SetTimer();
+
+            // Wait for the main method in Program to inform us that it's time to exit
+            _userExitEvent.WaitOne();
+            Log.Info("User has requested that the program exits. Stopping presence updates.");
+
+            // Stop anc clean up the timer
+            presenceUpdateTimer!.Stop();
+            presenceUpdateTimer.Dispose();
+        }
+
+        private void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            presenceUpdateTimer = new System.Timers.Timer(Program.StatusRefreshTimeInSeconds * 1000);
+            // Hook up the Elapsed event for the timer. 
+            presenceUpdateTimer.Elapsed += OnTimedEvent;
+            presenceUpdateTimer.AutoReset = true;
+            presenceUpdateTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Thread.CurrentThread.Name = "TimedPersistenceUpdate-" + Thread.CurrentThread.ManagedThreadId;
+            try
+            {
+                SetCurrentPresence(_discordRpcClient, _tf2Api);
+                // See https://stackoverflow.com/a/1650120/1687436 for determining equality between double and int
+                if (Math.Abs(presenceUpdateTimer!.Interval - Program.StatusRefreshTimeInSeconds) > 0.0000001)
+                {
+                    // We must've changed the interval previously.
+                    // It should only be a one-time operation so let's set it back to the correct value. 
+                    presenceUpdateTimer.Interval = Program.StatusRefreshTimeInSeconds;
+                }
+            }
+            catch (Exception exception)
+            {
+                if (exception is InvalidOperationException)
+                {
+                    Log.WarnFormat("Titanfall 2 process not found. Waiting {0} minute(s) and trying again.",
+                        ProcessOpenWaitTimeInMinutes);
+                    // Set the timer to wait longer
+                    presenceUpdateTimer!.Interval = ProcessOpenWaitTimeInMinutes * 60 * 1000;
+                }
+                else
+                {
+                    Log.Warn("Failed to perform Titanfall 2 rich presence update. Waiting " +
+                             Program.StatusRefreshTimeInSeconds + " seconds and trying again.", exception);
+                    // No need to change the timer interval before trying again
+                }
+            }
+        }
+
+        private static void SetCurrentPresence(DiscordRpcClient client, Titanfall2Api tf2Api)
+        {
+            var (gameDetails, gameState, timestamps, assets) = GetDetailsAndState(tf2Api);
+            //Set the rich presence
+            //Call this as many times as you want and anywhere in your code.
+            client.SetPresence(new RichPresence
+            {
+                Details = gameDetails,
+                State = gameState,
+                Timestamps = timestamps,
+                Assets = assets ?? new Assets
+                {
+                    LargeImageKey = "icon-900x900",
+                    LargeImageText = "Large Image Text",
+                }
+            });
+        }
+
+        private static (string, string, Timestamps?, Assets? assets) GetDetailsAndState(Titanfall2Api tf2Api)
+        {
+            string gameDetails = "";
+            string gameState = "";
+            Timestamps? timestamps = null;
+            Assets? assets = null;
+
+            if (tf2Api.GetGameModeAndMapName().Equals("Main Menu"))
+            {
+                gameDetails = "Main Menu";
+                timestamps = new Timestamps(Program.StartTimestamp);
+            }
+            else if (tf2Api.GetGameModeName().Contains("Campaign"))
+            {
+                gameDetails = "Campaign (" + tf2Api.GetSinglePlayerDifficulty() + ")";
+                gameState = tf2Api.GetFriendlyMapName();
+                timestamps = new Timestamps(Program.StartTimestamp);
+            }
+            else if (tf2Api.GetMultiplayerMapName().Equals("mp_lobby"))
+            {
+                gameDetails = "In a lobby";
+                timestamps = new Timestamps(Program.StartTimestamp);
+            }
+            // Besides mp_lobby, any mp map will be prefixed with mp_. Grab the specific details then!
+            else if (tf2Api.GetMultiplayerMapName().StartsWith("mp_"))
+            {
+                return GameDetailsProvider.GetMultiplayerDetails(tf2Api, Program.StartTimestamp);
+            }
+            // Could be main menu, might be some other random thing. This can be cleaned up later
+            else
+            {
+                gameDetails = tf2Api.GetGameModeName();
+                timestamps = new Timestamps(Program.StartTimestamp);
+            }
+
+            return (gameDetails, gameState, timestamps, assets);
+        }
+    }
+}

titanfall2-rp/Program.cs
@@ -1,110 +1,87 @@
 ﻿using System;
-using System;
-using System.Text.RegularExpressions;
+using System.IO;
+using System.Reflection;
 using System.Threading;
 using DiscordRPC;
 using DiscordRPC.Logging;
+using log4net;
+using log4net.Config;
 
 
 namespace titanfall2_rp
 {
-    class Program
+    static class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()!.DeclaringType);
+        private const string LoggerConfigFileName = "log4net.config";
         // For some reason, discord will show 4 hours as the starting time unless I add 4 hours here.
         // Seems the only way to fix this is to offset it here. I'm not sure if this is a timezone issue or what.
-        private static readonly DateTime StartTimestamp = DateTime.Now.AddHours(4);
-        private static readonly int StatusRefreshTimeInSeconds = 5;
+        public static readonly DateTime StartTimestamp = DateTime.Now.AddHours(4);
+        public const int StatusRefreshTimeInSeconds = 5;
+        private static AutoResetEvent _userRequestedExit = new AutoResetEvent(false);
+        private static readonly DiscordRpcClient DiscordRpcClient = new DiscordRpcClient("877931149740089374");
+        private static Thread? _presenceUpdatingThread;
 
         static void Main(string[] args)
         {
+            // Load logging configuration
+            // Thanks to https://jakubwajs.wordpress.com/2019/11/28/logging-with-log4net-in-net-core-3-0-console-app/
+            var loggerConfigFile = new FileInfo(LoggerConfigFileName);
+            if (!loggerConfigFile.Exists)
+            {
+                Console.WriteLine("Couldn't find '{0}'! Creating it (this only needs to happen once)...", LoggerConfigFileName);
+                File.WriteAllText(LoggerConfigFileName, Log4NetDefaultConfig.DefaultLog4NetConfig);
+            }
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            // Set this thread's name. This way it indicates which thread is the main thread (although this is usually 1)
+            Thread.CurrentThread.Name = "Main-" + Thread.CurrentThread.ManagedThreadId;
+
+            Log.Info("Starting Titanfall 2 Discord Rich Presence. Press enter at any time to exit!");
+
+            // Get an instance of Titanfall2Api. This doesn't do any initialization. That'll happen upon each method
+            // call. Meaning, the first method call will do any initializing.
             var tf2Api = new Titanfall2Api();
 
-            DiscordRpcClient client = new DiscordRpcClient("877931149740089374");
-            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            // Make the RPC library use a custom implemented logger (this is such a cool library feature)
+            DiscordRpcClient.Logger = new Log4NetDiscordLogger() { Level = LogLevel.Warning };
 
-            //Subscribe to events
-            client.OnReady += (sender, e) => { Console.WriteLine("Received Ready from user {0}", e.User.Username); };
+            // Subscribe to events
+            DiscordRpcClient.OnReady += (sender, e) => { Log.DebugFormat("Received Ready from user {0}", e.User.Username); };
+            // _discordRpcClient.OnClose += (sender, e) => { Log.DebugFormat("Received Close with code {0}. Reason: {1}", e.Code, e.Reason); };
+            // _discordRpcClient.OnError += (sender, message) => {Log.DebugFormat("Received error code {0}. Message was: {1}", message.Code, message.Message); };
+            DiscordRpcClient.OnPresenceUpdate += (sender, e) => { Log.DebugFormat("Received Update! {0}", e.Presence); };
 
-            client.OnPresenceUpdate += (sender, e) => { Console.WriteLine("Received Update! {0}", e.Presence); };
+            // Connect to the RPC
+            DiscordRpcClient.Initialize();
 
-            //Connect to the RPC
-            client.Initialize();
+            // Start the thread continuously updates the RP status
+            StartThread(DiscordRpcClient, tf2Api, _userRequestedExit);
 
+            // Waits for the user to hit enter. Then this will close
+            Console.ReadLine();
+            Log.Info("User has requested that the program exits.");
 
-            while (true)
-            {
-                try
-                {
-                    SetCurrentPresence(client, tf2Api);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    Console.WriteLine("Failed to perform Titanfall 2 rich presence update. Waiting " +
-                                      StatusRefreshTimeInSeconds + " seconds and trying again.");
-                }
-
-                Thread.Sleep(StatusRefreshTimeInSeconds * 1000);
-            }
-
-            client.Dispose();
+            // Cleanup
+            Log.Debug("Disposing of Discord RPC client");
+            DiscordRpcClient.Dispose();
+            Log.Debug("Informing the presence updating thread that the user has requested it's time to exit...");
+            _userRequestedExit.Set();
+            // Waits for the presence updating thread to close
+            _presenceUpdatingThread!.Join();
+            // Releases the resources used for the events (only after the thread that was using this has exited)
+            _userRequestedExit.Close();
+            Log.Info("Closing...");
         }
 
-        public static void SetCurrentPresence(DiscordRpcClient client, Titanfall2Api tf2Api)
+        // With some help from https://stackoverflow.com/a/10669337/1687436
+        private static void StartThread(DiscordRpcClient discordRpcClient, Titanfall2Api tf2Api, AutoResetEvent userExitEvent)
         {
-            var (gameDetails, gameState, timestamps, assets) = GetDetailsAndState(tf2Api);
-            //Set the rich presence
-            //Call this as many times as you want and anywhere in your code.
-            client.SetPresence(new RichPresence()
+            _presenceUpdatingThread = new Thread(new PresenceUpdateThread(discordRpcClient, tf2Api, userExitEvent).Run)
             {
-                Details = gameDetails,
-                State = gameState,
-                Timestamps = timestamps,
-                Assets = assets == null ? new Assets()
-                {
-                    LargeImageKey = "icon-900x900",
-                    LargeImageText = "Large Image Text",
-                    // SmallImageKey = "image_small"
-                } : assets
-            });
-        }
-
-        public static (string, string, Timestamps?, Assets? assets) GetDetailsAndState(Titanfall2Api tf2Api)
-        {
-            string gameDetails = "";
-            string gameState = "";
-            Timestamps? timestamps = null;
-            Assets? assets = null;
-
-            if (tf2Api.GetGameModeAndMapName().Equals("Main Menu"))
-            {
-                gameDetails = "Main Menu";
-                timestamps = new Timestamps(StartTimestamp);
-            }
-            else if (tf2Api.GetGameModeName().Contains("Campaign"))
-            {
-                gameDetails = "Campaign (" + tf2Api.GetSinglePlayerDifficulty() + ")";
-                gameState = tf2Api.GetFriendlyMapName();
-                timestamps = new Timestamps(StartTimestamp);
-            }
-            else if (tf2Api.GetMultiplayerMapName().Equals("mp_lobby"))
-            {
-                gameDetails = "In a lobby";
-                timestamps = new Timestamps(StartTimestamp);
-            }
-            // Besides mp_lobby, any mp map will be prefixed with mp_. Grab the specific details then!
-            else if (tf2Api.GetMultiplayerMapName().StartsWith("mp_"))
-            {
-                return GameDetailsProvider.GetMultiplayerDetails(tf2Api, StartTimestamp);
-            }
-            // Could be main menu, might be some other random thing. This can be cleaned up later
-            else
-            {
-                gameDetails = tf2Api.GetGameModeName();
-                timestamps = new Timestamps(StartTimestamp);
-            }
-
-            return (gameDetails, gameState, timestamps, assets);
+                Name = "Discord RP Updating Thread"
+            };
+            _presenceUpdatingThread.Start();
         }
     }
 }

titanfall2-rp/Log4NetDefaultConfig.cs
@@ -1,0 +1,53 @@
+﻿namespace titanfall2_rp
+{
+    public static class Log4NetDefaultConfig
+    {
+        /// <summary>
+        /// In case the user doesn't have the log4net config file next to the exe. Make one for them using this string
+        /// </summary>
+        public static readonly string DefaultLog4NetConfig = @"<log4net>
+	<root>
+		<!--Set this to ALL to see all log levels-->
+		<level value=""INFO"" />
+		<appender-ref ref=""console"" />
+		<appender-ref ref=""file"" />
+	</root>
+
+	<!--File Appender-->
+	<appender name=""file"" type=""log4net.Appender.RollingFileAppender"">
+		<file value=""titanfall-rp.log"" />
+		<appendToFile value=""true"" />
+		<rollingStyle value=""Size"" />
+		<maxSizeRollBackups value=""5"" />
+		<maximumFileSize value=""25MB"" />
+		<staticLogFileName value=""true"" />
+		<layout type=""log4net.Layout.PatternLayout"">
+			<conversionPattern value=""%date [%thread] %level %logger - %message%newline"" />
+		</layout>
+	</appender>
+
+	<!--Console appender-->
+	<appender name=""console"" type=""log4net.Appender.ManagedColoredConsoleAppender"">
+		<mapping>
+			<level value=""INFO"" />
+			<forecolor value=""Green"" />
+		</mapping>
+		<mapping>
+			<level value=""WARN"" />
+			<forecolor value=""Yellow"" />
+		</mapping>
+		<mapping>
+			<level value=""ERROR"" />
+			<forecolor value=""Red"" />
+		</mapping>
+		<mapping>
+			<level value=""DEBUG"" />
+			<forecolor value=""Blue"" />
+		</mapping>
+		<layout type=""log4net.Layout.PatternLayout"">
+			<conversionpattern value=""%date [%thread] %-5level - %message%newline"" />
+		</layout>
+	</appender>
+</log4net>";
+    }
+}

titanfall2-rp/Log4NetDiscordLogger.cs
@@ -1,0 +1,45 @@
+﻿using System.Reflection;
+using DiscordRPC.Logging;
+using log4net;
+
+namespace titanfall2_rp
+{
+    public class Log4NetDiscordLogger : ILogger
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()!.DeclaringType);
+
+        public void Trace(string message, params object[] args)
+        {
+            if (Level <= LogLevel.Trace)
+            {
+                Log.DebugFormat(message, args);
+            }
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            if (Level <= LogLevel.Info)
+            {
+                Log.InfoFormat(message, args);
+            }
+        }
+
+        public void Warning(string message, params object[] args)
+        {
+            if (Level <= LogLevel.Warning)
+            {
+                Log.WarnFormat(message, args);
+            }
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            if (Level <= LogLevel.Error)
+            {
+                Log.ErrorFormat(message, args);
+            }
+        }
+
+        public LogLevel Level { get; set; }
+    }
+}

titanfall2-rp/ProcessNetApi.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using log4net;
 using Process.NET;
 using Process.NET.Memory;
 
@@ -6,6 +8,7 @@ namespace titanfall2_rp
 {
     public static class ProcessNetApi
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()!.DeclaringType);
         private static ProcessSharp? _sharp;
         private const string ProcessName = "Titanfall2";
 
@@ -16,21 +19,19 @@ namespace titanfall2_rp
 
         public static bool Init()
         {
-            Console.WriteLine("Initializing ProcessNetApi...");
-            Console.WriteLine("Searching for process '" + ProcessName + "'...");
+            Log.Debug("Initializing ProcessNetApi...");
+            Log.Debug("Searching for process '" + ProcessName + "'...");
             System.Diagnostics.Process[] processSearch = System.Diagnostics.Process.GetProcessesByName(ProcessName);
             if (processSearch.Length == 0)
             {
-                Console.WriteLine("'" + ProcessName + "'" + " isn't running!");
+                Log.Debug("'" + ProcessName + "'" + " isn't running!");
                 return false;
             }
-            else
-            {
-                Console.WriteLine("Found '" + ProcessName + "'" + "!");
-                var proc = processSearch[0];
-                _sharp = new ProcessSharp(proc, MemoryType.Remote);
-                return true;
-            }
+
+            Log.Debug("Found '" + ProcessName + "'" + "!");
+            var proc = processSearch[0];
+            _sharp = new ProcessSharp(proc, MemoryType.Remote);
+            return true;
         }
 
         public static ProcessSharp GetProcess()
@@ -40,14 +41,11 @@ namespace titanfall2_rp
                 throw new NullReferenceException(
                     "The Init() method was never called before attempting to access the process.");
             }
-            else if (_sharp.Native.HasExited)
+            if (_sharp.Native.HasExited)
             {
                 throw new AccessViolationException("Attempted to access a process that has already exited.");
             }
-            else
-            {
-                return _sharp;
-            }
+            return _sharp;
         }
     }
 }

titanfall2-rp/Titanfall2API.cs
@@ -6,6 +6,8 @@ using static titanfall2_rp.ProcessApi;
 
 namespace titanfall2_rp
 {
+    // This is probably not thread safe. Multiple threads could potentially attempt to initialize this class.
+    // However, this is unlikely given that the presence update time is multiple seconds.
     public partial class Titanfall2Api
     {
         private static readonly Regex GameModeAndMapRegex = new Regex("Playing (.*) on (.*)");

titanfall2-rp/log4net.config
@@ -1,0 +1,44 @@
+﻿<log4net>
+	<root>
+		<!--Set this to ALL to see all log levels-->
+		<level value="INFO" />
+		<appender-ref ref="console" />
+		<appender-ref ref="file" />
+	</root>
+
+	<!--File Appender-->
+	<appender name="file" type="log4net.Appender.RollingFileAppender">
+		<file value="titanfall-rp.log" />
+		<appendToFile value="true" />
+		<rollingStyle value="Size" />
+		<maxSizeRollBackups value="5" />
+		<maximumFileSize value="25MB" />
+		<staticLogFileName value="true" />
+		<layout type="log4net.Layout.PatternLayout">
+			<conversionPattern value="%date [%thread] %level %logger - %message%newline" />
+		</layout>
+	</appender>
+
+	<!--Console appender-->
+	<appender name="console" type="log4net.Appender.ManagedColoredConsoleAppender">
+		<mapping>
+			<level value="INFO" />
+			<forecolor value="Green" />
+		</mapping>
+		<mapping>
+			<level value="WARN" />
+			<forecolor value="Yellow" />
+		</mapping>
+		<mapping>
+			<level value="ERROR" />
+			<forecolor value="Red" />
+		</mapping>
+		<mapping>
+			<level value="DEBUG" />
+			<forecolor value="Blue" />
+		</mapping>
+		<layout type="log4net.Layout.PatternLayout">
+			<conversionpattern value="%date [%thread] %-5level - %message%newline" />
+		</layout>
+	</appender>
+</log4net>

titanfall2-rp/titanfall2-rp.csproj
@@ -19,7 +19,14 @@
 
     <ItemGroup>
       <PackageReference Include="DiscordRichPresence" Version="1.0.175" />
+      <PackageReference Include="log4net" Version="2.0.12" />
       <PackageReference Include="Process.NET" Version="1.0.8" />
+    </ItemGroup>
+
+    <ItemGroup>
+      <None Update="log4net.config">
+        <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+      </None>
     </ItemGroup>
 
 </Project>

commit 639a177f30eb6145c2f0961bf25bc5737c4d84a6

Fixed the presence trying to update every 5 ms (#52)
Fixed a problem introduced in #47.

titanfall2-rp/PresenceUpdateThread.cs
@@ -40,7 +40,7 @@ namespace titanfall2_rp
         private void SetTimer()
         {
             // Create a timer with a two second interval.
-            presenceUpdateTimer = new System.Timers.Timer(Program.StatusRefreshTimeInSeconds * 1000);
+            presenceUpdateTimer = new System.Timers.Timer(Program.StatusRefreshTimeInMs);
             // Hook up the Elapsed event for the timer. 
             presenceUpdateTimer.Elapsed += OnTimedEvent;
             presenceUpdateTimer.AutoReset = true;
@@ -54,11 +54,11 @@ namespace titanfall2_rp
             {
                 SetCurrentPresence(_discordRpcClient, _tf2Api);
                 // See https://stackoverflow.com/a/1650120/1687436 for determining equality between double and int
-                if (Math.Abs(presenceUpdateTimer!.Interval - Program.StatusRefreshTimeInSeconds) > 0.0000001)
+                if (Math.Abs(presenceUpdateTimer!.Interval - Program.StatusRefreshTimeInMs) > 0.0000001)
                 {
                     // We must've changed the interval previously.
                     // It should only be a one-time operation so let's set it back to the correct value. 
-                    presenceUpdateTimer.Interval = Program.StatusRefreshTimeInSeconds;
+                    presenceUpdateTimer.Interval = Program.StatusRefreshTimeInMs;
                 }
             }
             catch (Exception exception)

titanfall2-rp/Program.cs
@@ -18,6 +18,7 @@ namespace titanfall2_rp
         // Seems the only way to fix this is to offset it here. I'm not sure if this is a timezone issue or what.
         public static readonly DateTime StartTimestamp = DateTime.Now.AddHours(4);
         public const int StatusRefreshTimeInSeconds = 5;
+        public const int StatusRefreshTimeInMs = StatusRefreshTimeInSeconds * 1000;
         private static AutoResetEvent _userRequestedExit = new AutoResetEvent(false);
         private static readonly DiscordRpcClient DiscordRpcClient = new DiscordRpcClient("877931149740089374");
         private static Thread? _presenceUpdatingThread;