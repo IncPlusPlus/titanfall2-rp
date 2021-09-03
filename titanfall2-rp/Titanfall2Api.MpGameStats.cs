595fe1e80735be446073e04ac4042923190c7e36
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-09-03 08:23:52 -0400

    Add stubs for remaining goals

--- titanfall2-rp/Titanfall2Api.MpGameStats.cs
+++ titanfall2-rp/Titanfall2Api.MpGameStats.cs
@@ -167,6 +167,33 @@ namespace titanfall2_rp
                 {
                     return _tf2Api._sharp!.Memory.Read<int>(_tf2Api._engineDllBaseAddress + 0x11218CA0);
                 }
+
+                /// <summary>
+                /// Get the score of the current user.
+                /// </summary>
+                /// <returns>the user's score</returns>
+                public int GetMyScore()
+                {
+                    throw new NotImplementedException(HelpMeBruh);
+                }
+
+                /// <summary>
+                /// Get the bonus of the current user.
+                /// </summary>
+                /// <returns>the user's bonus</returns>
+                public int GetMyBonus()
+                {
+                    throw new NotImplementedException(HelpMeBruh);
+                }
+
+                /// <summary>
+                /// Get the kills of the current user.
+                /// </summary>
+                /// <returns>the user's kills</returns>
+                public int GetMyKills()
+                {
+                    throw new NotImplementedException(HelpMeBruh);
+                }
             }
 
             public class CaptureTheFlag
