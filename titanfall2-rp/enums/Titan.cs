b1fa9b7beb93cccc6d79d7f1ae79b47978b8cb13
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-09-07 02:17:28 -0400

    Added Titan enum

---
+++ titanfall2-rp/enums/Titan.cs
@@ -0,0 +1,32 @@
+﻿namespace titanfall2_rp.enums
+{
+    public enum Titan
+    {
+        Ion,
+        Scorch,
+        Northstar,
+        // ReSharper disable once IdentifierTypo
+        Ronin,
+        Tone,
+        Legion,
+        Monarch,
+    }
+
+    internal static class TitanMethods
+    {
+        public static Titan GetTitan(int titanValue)
+        {
+            return (Titan)titanValue;
+        }
+
+        public static string ToFriendlyString(this Titan titan)
+        {
+            return titan.ToString();
+        }
+
+        public static string GetAssetName(this Titan titan)
+        {
+            return titan.ToString();
+        }
+    }
+}
