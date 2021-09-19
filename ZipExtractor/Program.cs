000483c4ae5bdc3a5752bf850908ae6b934612a8
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-09-19 10:53:36 -0400

    Attempt to embed an EXE

---
+++ ZipExtractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZipExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- titanfall2-rp/updater/WineUpdater.cs
+++ titanfall2-rp/updater/WineUpdater.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 using AutoUpdaterDotNET;
@@ -28,12 +29,27 @@ namespace titanfall2_rp.updater
         {
             XmlTextReader xmlTextReader = new XmlTextReader(new StringReader(_client.GetStringAsync(_appCastUri).Result)) { XmlResolver = null };
             UpdateInfoEventArgs args = (UpdateInfoEventArgs)xmlSerializer.Deserialize(xmlTextReader)!;
-            return null;
+            return new Version(args.CurrentVersion) > AppVersion;
         }
 
         protected override void AttemptUpdate()
         {
-            throw new NotImplementedException();
+            XmlTextReader xmlTextReader = new XmlTextReader(new StringReader(_client.GetStringAsync(_appCastUri).Result)) { XmlResolver = null };
+            UpdateInfoEventArgs args = (UpdateInfoEventArgs)xmlSerializer.Deserialize(xmlTextReader)!;
+            var tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".tmp");
+            Uri dlUri = new Uri(args.DownloadURL);
+            _client.DownloadFileTaskAsync(dlUri, tempFile).RunSynchronously();
+        }
+    }
+
+    public static class HttpClientUtils
+    {
+        public static async Task DownloadFileTaskAsync(this HttpClient client, Uri uri, string fileName)
+        {
+            // https://stackoverflow.com/a/66270371/1687436
+            await using var s = await client.GetStreamAsync(uri);
+            await using var fs = new FileStream(fileName, FileMode.CreateNew);
+            await s.CopyToAsync(fs);
         }
     }
 }
