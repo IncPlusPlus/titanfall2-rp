04efd58c8519ee202b3952810389e6415f48d962
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-09-19 19:54:41 -0400

    Enable embedding and write some of the updater stuff

--- titanfall2-rp/updater/WineUpdater.cs
+++ titanfall2-rp/updater/WineUpdater.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
@@ -11,6 +15,10 @@ using log4net;
 
 namespace titanfall2_rp.updater
 {
+    /// <summary>
+    /// An updater that doesn't reference WinForms and therefore works in Wine.
+    /// This is largely adapted from the AutoUpdater.NET updater code.
+    /// </summary>
     public class WineUpdater : UpdateHelper
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod()!.DeclaringType);
@@ -27,27 +35,62 @@ namespace titanfall2_rp.updater
 
         protected override bool? CheckForUpdates()
         {
-            XmlTextReader xmlTextReader = new XmlTextReader(new StringReader(_client.GetStringAsync(_appCastUri).Result)) { XmlResolver = null };
+            // TODO: Try cleaning up the zip extractor and any previous downloads here (be nice to %tmp%)
+            XmlTextReader xmlTextReader =
+                new XmlTextReader(new StringReader(_client.GetStringAsync(_appCastUri).Result)) { XmlResolver = null };
             UpdateInfoEventArgs args = (UpdateInfoEventArgs)xmlSerializer.Deserialize(xmlTextReader)!;
             return new Version(args.CurrentVersion) > AppVersion;
         }
 
         protected override void AttemptUpdate()
         {
-            XmlTextReader xmlTextReader = new XmlTextReader(new StringReader(_client.GetStringAsync(_appCastUri).Result)) { XmlResolver = null };
+            XmlTextReader xmlTextReader = new(new StringReader(_client.GetStringAsync(_appCastUri).Result))
+                { XmlResolver = null };
             UpdateInfoEventArgs args = (UpdateInfoEventArgs)xmlSerializer.Deserialize(xmlTextReader)!;
-            var tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".tmp");
+            var tempFile = Path.Combine(Path.GetTempPath(), "titanfall2-rp-update-" + Guid.NewGuid() + ".tmp");
             Uri dlUri = new Uri(args.DownloadURL);
-            _client.DownloadFileTaskAsync(dlUri, tempFile).RunSynchronously();
-        }
-    }
+            var response = _client.GetAsync(dlUri).Result;
+            StreamToFile(response.Content.ReadAsStream(), tempFile).Wait();
 
-    public static class HttpClientUtils
-    {
-        public static async Task DownloadFileTaskAsync(this HttpClient client, Uri uri, string fileName)
+            var contentDisposition = response.Content.Headers.ContentDisposition;
+
+            var fileName = string.IsNullOrEmpty(contentDisposition?.FileName)
+                ? Path.GetFileName(response.RequestMessage!.RequestUri!.LocalPath)
+                : contentDisposition.FileName!;
+            var tempPath = Path.Combine(Path.GetTempPath(), fileName);
+
+            var extension = Path.GetExtension(tempPath);
+            if (extension.Equals(".zip", StringComparison.OrdinalIgnoreCase))
+            {
+                var installerPath = Path.Combine(Path.GetTempPath(), "WineZipExtractor.exe");
+                File.WriteAllBytes(installerPath, Properties.Resources.ZipExtractor);
+
+                var executablePath = System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName;
+                var extractionPath = Path.GetDirectoryName(executablePath);
+
+                var arguments = new StringBuilder($"\"{tempPath}\" \"{extractionPath}\" \"{executablePath}\"");
+
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = installerPath,
+                    UseShellExecute = true,
+                    Arguments = arguments.ToString(),
+                };
+                
+                var proc = System.Diagnostics.Process.Start(processStartInfo);
+                
+                Environment.Exit(0);
+            }
+            else
+            {
+                throw new ArgumentException($"Unable to install using a file with the extension '{extension}'");
+            }
+        }
+
+        public static async Task StreamToFile(Stream s, string fileName)
         {
             // https://stackoverflow.com/a/66270371/1687436
-            await using var s = await client.GetStreamAsync(uri);
+            // await using var s = await client.GetStreamAsync(uri);
             await using var fs = new FileStream(fileName, FileMode.CreateNew);
             await s.CopyToAsync(fs);
         }
