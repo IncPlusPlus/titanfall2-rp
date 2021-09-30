8353cd4bcb0e418ce8ccfe186b1691d180b06c97
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-09-30 16:19:37 -0400

    Add stub implementations for LP, PvP, and LTS

--- titanfall2-rp/MpGameStats/LastTitanStanding.cs
+++ titanfall2-rp/MpGameStats/LastTitanStanding.cs
@@ -1,10 +1,42 @@
-﻿using Process.NET;
+﻿using System;
+using Process.NET;
 
 namespace titanfall2_rp.MpGameStats
 {
     public class LastTitanStanding : MpStats
     {
         public LastTitanStanding(Titanfall2Api tf2Api, ProcessSharp processSharp) : base(tf2Api, processSharp)
-        { }
+        {
+        }
+
+        /// <returns>the current round number</returns>
+        public int GetRoundNumber()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
+
+        /// <returns>the total number of rounds (to give the current round number some context)</returns>
+        public int GetTotalRounds()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
+
+        /// <returns>the time remaining in the current round (measured in seconds)</returns>
+        public int GetTimeRemainingInRoundInSeconds()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
+
+        /// <returns>the number of titans the user has killed</returns>
+        public int GetMyTitanKills()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
+
+        /// <returns>the amount of damage the user has done to other titans</returns>
+        public int GetMyTitanDamage()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
     }
 }
--- titanfall2-rp/MpGameStats/LiveFire.cs
+++ titanfall2-rp/MpGameStats/LiveFire.cs
@@ -1,10 +1,39 @@
-﻿using Process.NET;
+﻿using System;
+using Process.NET;
 
 namespace titanfall2_rp.MpGameStats
 {
     public class LiveFire : MpStats
     {
         public LiveFire(Titanfall2Api tf2Api, ProcessSharp processSharp) : base(tf2Api, processSharp)
-        { }
+        {
+        }
+
+        /// <summary>
+        /// Get the number of players the user has killed
+        /// </summary>
+        /// <returns>the number of players the user has killed</returns>
+        public int GetMyKills()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
+
+        /// <summary>
+        /// Get the number of flags the user has secured
+        /// </summary>
+        /// <returns>the number of flags the user has secured</returns>
+        public int GetMyFlags()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
+
+        /// <summary>
+        /// Get the number of times the user has died
+        /// </summary>
+        /// <returns>the number of times the user has died</returns>
+        public int GetMyDeaths()
+        {
+            throw new NotImplementedException(HelpMeBruh);
+        }
     }
 }
