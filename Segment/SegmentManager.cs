17fb227a3fb15628931264ff770034101035a49f
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-10-01 12:11:33 -0400

    Add a new project for Segment

---
+++ Segment/SegmentManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Segment
+{
+    public class SegmentManager
+    {
+        private const string WriteKey = "DAXSzUgf2tu57mPFgnWSggNyD3v4BEhC";
+        private static bool EnableSegment = true;
+
+        public static void Initialize()
+        {
+            
+        }
+    }
+}
