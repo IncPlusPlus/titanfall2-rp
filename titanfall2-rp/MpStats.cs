44f0fa84c75ddd090a7f32b80d72d03a0dc6d873
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-12-30 00:12:57 -0500

    Add Fastball stub

--- titanfall2-rp/MpStats.cs
+++ titanfall2-rp/MpStats.cs
@@ -203,6 +203,7 @@ namespace titanfall2_rp
                 GameMode.fd_insane => new FrontierDefense(titanfall2Api, sharp),
                 GameMode.solo => throw new ArgumentException("Tried to get multiplayer details for the campaign"),
                 GameMode.ffa => new FreeForAll(titanfall2Api, sharp),
+                GameMode.fastball => new Fastball(titanfall2Api, sharp),
                 _ => ReportGameModeFailure(gameMode)
             };
         }
