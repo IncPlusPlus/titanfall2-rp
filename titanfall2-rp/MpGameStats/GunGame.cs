96caaef73c514cec47578ac06fa94869fa1b11f1
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-12-29 23:51:56 -0500

    Add GunGame stub

---
+++ titanfall2-rp/MpGameStats/GunGame.cs
@@ -0,0 +1,13 @@
+﻿using Process.NET;
+
+namespace titanfall2_rp.MpGameStats
+{
+    public class GunGame : MpStats
+    {
+        public GunGame(Titanfall2Api tf2Api, ProcessSharp processSharp) : base(tf2Api, processSharp)
+        {
+        }
+
+        // TODO: Figure out how to show Gun Game scores
+    }
+}
