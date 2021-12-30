81eb7d54cbccc42e6bfa4a75d72bf6e2d74b499a
Ryan Cloherty <cloherty.ryan@gmail.com>
2021-12-29 23:40:43 -0500

    Add FreeAgents stub

---
+++ titanfall2-rp/MpGameStats/FreeAgents.cs
@@ -0,0 +1,13 @@
+﻿using Process.NET;
+
+namespace titanfall2_rp.MpGameStats
+{
+    public class FreeAgents : MpStats
+    {
+        public FreeAgents(Titanfall2Api tf2Api, ProcessSharp processSharp) : base(tf2Api, processSharp)
+        {
+        }
+
+        // TODO: Figure out how scores work for this gamemode
+    }
+}
