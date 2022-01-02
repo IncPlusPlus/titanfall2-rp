a9d1203b0fe019f7b8ed4e48df3afd791cc7344f
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-01-02 01:22:02 -0500

    Add Free Agents implementation

--- titanfall2-rp/MpGameStats/FreeAgents.cs
+++ titanfall2-rp/MpGameStats/FreeAgents.cs
@@ -8,6 +8,22 @@ namespace titanfall2_rp.MpGameStats
         {
         }
 
-        // TODO: Figure out how scores work for this gamemode
+        /// <summary>
+        /// The score of "my team" is actually just the current user's score
+        /// </summary>
+        /// <returns>the current user's score</returns>
+        protected override int GetMyTeamScore()
+        {
+            return GetScore();
+        }
+
+        /// <summary>
+        /// The score of the "enemy team" is actually the score of the player with the highest score that isn't myself
+        /// </summary>
+        /// <returns>the score of the player with the highest score that isn't myself</returns>
+        protected override int GetEnemyTeamScore()
+        {
+            return GetHighestScoreInGame(GetMyIdOnServer());
+        }
     }
 }
