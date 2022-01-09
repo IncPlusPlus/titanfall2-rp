1797b66370c0c7571f1d4adab13336cec9a66e42
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-01-09 18:59:42 -0500

    Show Infection game info (#190)

--- titanfall2-rp/MpStats.cs
+++ titanfall2-rp/MpStats.cs
@@ -268,6 +268,7 @@ namespace titanfall2_rp
                 GameMode.hidden => new TheHidden(titanfall2Api, sharp),
                 GameMode.gg => new GunGame(titanfall2Api, sharp),
                 GameMode.tt => new TitanTag(titanfall2Api, sharp),
+                GameMode.inf => new Infection(titanfall2Api, sharp),
                 _ => ReportGameModeFailure(gameMode, titanfall2Api, sharp)
             };
         }
